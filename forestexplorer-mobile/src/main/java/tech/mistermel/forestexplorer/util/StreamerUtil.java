df017f16737fcd150d64109a440c106479738a20
Melle Moerkerk <melle@mistermel.tech>
2020-06-14 17:38:55 +0200

    Implement StreamerUtil
    Not tested on RPi, but works on Windows

--- forestexplorer-mobile/src/main/java/tech/mistermel/forestexplorer/util/StreamerUtil.java
+++ forestexplorer-mobile/src/main/java/tech/mistermel/forestexplorer/util/StreamerUtil.java
@@ -1,5 +1,7 @@
 package tech.mistermel.forestexplorer.util;
 
+import java.io.IOException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,36 +10,35 @@ public class StreamerUtil {
 	private StreamerUtil() {}
 	
 	private static final Logger logger = LoggerFactory.getLogger(StreamerUtil.class);
-	private static final String CMD = "gst-launch-1.0 -v v4l2src device=/dev/video0 ! \"image/jpeg, width=1280, height=720, framerate=15/1\" ! rtpjpegpay ! udpsink host=192.168.178.3 port=5001";
+	private static final String CMD = "bash -c \"gst-launch-1.0 -v v4l2src device=/dev/video0 ! \\\"image/jpeg, width=1280, height=720, framerate=15/1\\\" ! rtpjpegpay ! udpsink host=192.168.178.3 port=5001\"";
 	
 	private static Process process;
 	
 	public static void startStreamer() {
-		logger.warn("startStreamer() not implemented");
-		/*if(process != null)
+		if(isStreamerActive())
 			return;
 		
 		logger.info("Starting streamer");
-		ProcessBuilder pb = new ProcessBuilder(CMD.split(" ")).inheritIO();
-		
+
 		try {
-			process = pb.start();
+			process = new ProcessBuilder(CMD.split(" "))
+					.inheritIO()
+					.start();
 		} catch (IOException e) {
 			logger.error("Error while starting streaming screen", e);
-		}*/
+		}
 	}
 	
 	public static void stopStreamer() {
-		logger.warn("stopStreamer() not implemented");
-		/*if(process == null)
+		if(!isStreamerActive())
 			return;
 		
 		logger.info("Exiting streamer");
-		process.destroy();*/
+		process.destroy();
 	}
 	
 	public static boolean isStreamerActive() {
-		return process != null;
+		return process != null && process.isAlive();
 	}
 	
 }
