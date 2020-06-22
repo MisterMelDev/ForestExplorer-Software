be80e4b982448f28754270a84f1d910ecf8f58d9
Melle Moerkerk <melle@mistermel.tech>
2020-06-22 10:42:38 +0200

    Added getDistance method to NavigationUtil
    This was taken from Apache Lucene's SloppyMath class

--- forestexplorer-mobile/src/main/java/tech/mistermel/forestexplorer/util/NavigationUtil.java
+++ forestexplorer-mobile/src/main/java/tech/mistermel/forestexplorer/util/NavigationUtil.java
@@ -18,4 +18,24 @@ public class NavigationUtil {
 		return Math.toDegrees((radians + Math.PI) % Math.PI);
 	}
 	
+	/*
+	This snippet was taken from Apache Lucene's SloppyMath class
+	License: https://github.com/apache/lucene-solr/blob/master/LICENSE
+	*/
+	
+	private static final double TO_METERS = 6_371_008.7714D;
+	
+	public static double getDistance(LatLng loc, Waypoint dstWaypoint) {
+		LatLng dst = dstWaypoint.getLocation();
+		
+		double x1 = Math.toRadians(loc.getLatitude());
+		double x2 = Math.toRadians(dst.getLatitude());
+		double h1 = 1 - Math.cos(x1 - x2);
+		double h2 = 1 -Math.cos(Math.toRadians(loc.getLongitude() - dst.getLongitude()));
+		double h = h1 + Math.cos(x1) * Math.cos(x2) * h2;
+		double sortKey = Double.longBitsToDouble(Double.doubleToRawLongBits(h) & 0xFFFFFFFFFFFFFFF8L);
+		
+		return TO_METERS * 2 * Math.asin(Math.min(1, Math.sqrt(sortKey * 0.5)));
+	}
+	
 }
