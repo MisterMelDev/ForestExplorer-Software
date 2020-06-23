0a1a2e663b8b74d735c8738c9331599fa882ab88
Melle Moerkerk <melle@mistermel.tech>
2020-06-23 18:38:14 +0200

    Added calibration sketch to project

---
+++ CalibrationEEPROM/CalibrationEEPROM.ino
@@ -0,0 +1,29 @@
+#include "MPU9250.h"
+#include "eeprom_utils.h"
+
+MPU9250 mpu;
+
+void setup()
+{
+    Serial.begin(115200);
+
+    Wire.begin();
+
+    delay(2000);
+    mpu.setup(Wire);
+
+    delay(5000);
+
+    // calibrate when you want to
+    mpu.calibrateAccelGyro();
+    mpu.calibrateMag();
+
+    // save to eeprom
+    saveCalibration();
+
+    mpu.printCalibration();
+}
+
+void loop()
+{
+}
